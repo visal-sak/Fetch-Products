6f59800b8550361e685bdaeda9b1322e91fe2df5
visal sak <113172768+sakvisal@users.noreply.github.com>
2023-05-25 10:27:23 +0700

    update new

--- src/app/form/page.js
+++ src/app/form/page.js
@@ -152,7 +152,7 @@ Swal.fire({
                     type="text"
                     name="title"
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="name@company.com"
+                    placeholder="input your products's name"
                   />
                   <ErrorMessage name="title">
                     {(msg) => <div className="text-red-600">{msg}</div>}
@@ -169,7 +169,7 @@ Swal.fire({
                     type="text"
                     name="price"
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="name@company.com"
+                    placeholder="Your price"
                     required=""
                   />
                   <ErrorMessage name="price">
@@ -187,7 +187,7 @@ Swal.fire({
                     type="text"
                     name="description"
                     id="description"
-                    placeholder="-----------"
+                    placeholder="Your Description"
                     className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     required=""
                   />
